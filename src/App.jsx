8db70b59003ef3d10c2ae78fc405d3629a624691
Abo_Tolba <122931774+AhmedElbassuony@users.noreply.github.com>
2025-05-09 08:22:18 +0300

    Task Provider Contect

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,53 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { createContext, useReducer } from "react";
+
+
+const initialTodos = {
+  filter: "All",
+  data: [{
+    task: "Task 1",
+    done: false
+  }, {
+    task: "Task 2",
+    done: true
+  }, {
+    task: "Task 3",
+    done: false
+  }
+  ]
+};
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "toggle":
+      return {
+        ...state, data: state.data.map((task, index) => {
+          if (index === action.idx) {
+            return { ...task, done: !task.done }
+          }
+          return task;
+        })
+      }
+    case "All":
+      return { ...state, filter: "All" };
+    case "Pending":
+      return { ...state, filter: "Pending" };
+    case "Complete":
+      return { ...state, filter: "Complete" };
+    default:
+      return state;
+  }
+};
+
+export const TaskProvider = createContext();
+
 
 function App() {
-  const [count, setCount] = useState(0)
 
+  const [tasks, dispatch] = useReducer(reducer, initialTodos);
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <TaskProvider.Provider value={{ tasks: tasks, dispatch: dispatch }}>
+
+    </TaskProvider.Provider>
   )
 }
 
